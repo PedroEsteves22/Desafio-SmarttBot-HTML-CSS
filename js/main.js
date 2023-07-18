f2ee363da5d010d951654f0cccef3b5e3e2eb7fc
PedroEsteves22 <pesteves458@gmail.com>
2023-07-18 00:25:37 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const botaoAbrir = document.querySelector('[data-modal="abrir"]');
+const botaoFechar = document.querySelector('[data-modal="fechar"]');
+const containerModal = document.querySelector('[data-modal="container"]');
+
+function abrirModal(event){
+  event.preventDefault();
+  containerModal.classList.add('ativo');
+}
+
+function fecharModal(event){
+  event.preventDefault();
+  containerModal.classList.remove('ativo');
+}
+
+function cliqueForaModal(event){
+  fecharModal();
+}
+
+botaoAbrir.addEventListener('click', abrirModal);
+
+botaoFechar.addEventListener('click', fecharModal);
